9d183e2fb5d0b02311b43f82f9af0eb59f225f4c
anand <anand.k4756@gmail.com>
2025-10-23 15:35:59 +0530

    added more messages

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ app.post("/user/booking-confirmation", async (req, res) => {
       booking_id,
       extra_km_charge,
     } = req.body;
+    remainingAmount = Number(req.body.remainingAmount).toFixed(2);
+    distance = Number(req.body.distance).toFixed(2);
+    extra_km_charge = Number(req.body.extra_km_charge).toFixed(2);
+
     await sendSMS("paymentConfirmation", phoneNumber, [
       name,
       payment_amount,
@@ -41,6 +45,130 @@ app.post("/user/booking-confirmation", async (req, res) => {
     res.status(400).json({ success: false });
   }
 });
+app.post("/user/booking-accept", async (req, res) => {
+  try {
+    const {
+      phoneNumber,
+      name,
+      driver_phone_number,
+      cab_number,
+      pickup_date_time,
+      booking_id,
+      cab,
+      driver_name,
+    } = req.body;
+    await sendSMS("bookingConfirmation", phoneNumber, [
+      name,
+      booking_id,
+      driver_name, //driver name take
+    ]);
+    await sendSMS("cabDetails", phoneNumber, [
+      driver_phone_number,
+      cab_number,
+      pickup_date_time,
+    ]);
+
+    await sendSMS("bookingDriverAccepted", driver_phone_number, [
+      booking_id,
+      name,
+      phoneNumber,
+    ]);
+    await sendSMS("bookingDriverAccepted2", driver_phone_number, [
+      cab, // cab nname
+      cab_number,
+      pickup_date_time,
+      // phoneNumber,
+    ]);
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ success: false });
+  }
+});
+
+app.post("/user/booking-complete", async (req, res) => {
+  try {
+    const {
+      phoneNumber,
+      // gst_amount,
+      driver_phone_number,
+      sub_total,
+      amount_to_pay,
+      booking_id,
+      extra_km,
+      extra_days,
+      user_name,
+      // cab,
+      // cab_number,
+      // pickup_date_time,
+    } = req.body;
+    let gstamount = (sub_total * 5) / 100;
+    await sendSMS("TripSummary", phoneNumber, [
+      booking_id,
+      extra_km,
+      extra_days,
+    ]);
+    await sendSMS("TripSummaryDetailed", phoneNumber, [
+      sub_total,
+      gstamount,
+      amount_to_pay,
+    ]);
+
+    await sendSMS("bookingDriverCompleted", driver_phone_number, [
+      booking_id,
+      user_name,
+      phoneNumber,
+    ]);
+    await sendSMS("bookingDriverCompleted2", driver_phone_number, [
+      extra_km + " KM " + extra_days + " Days",
+
+      amount_to_pay,
+      "0.0",
+    ]);
+    // await sendSMS("bookingDriverAccepted", driver_phone_number, [
+    //   booking_id,
+    //   user_name,
+    //   phoneNumber,
+    // ]);
+    // await sendSMS("bookingDriverAccepted2", driver_phone_number, [
+    //   cab,
+    //   cab_number,
+    //   pickup_date_time,
+    //   phoneNumber,
+    // ]);
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ success: false });
+  }
+});
+
+app.post("/driver/registration", async (req, res) => {
+  try {
+    const { phoneNumber, driver_name } = req.body;
+    await sendSMS("driverRegistration", phoneNumber, [
+      driver_name,
+      process.env.SUPPORT_CONTACT,
+    ]);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ success: false });
+  }
+});
+
+app.post("/user/profile_completed", async (req, res) => {
+  try {
+    const { phoneNumber, userName } = req.body;
+    await sendSMS("userProfileCompleted", phoneNumber, [
+      userName,
+      process.env.SUPPORT_CONTACT,
+    ]);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ success: false });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- template.js
+++ template.js
@@ -9,9 +9,14 @@ const templates = {
     senderId: "SATICB",
     text: "Dear {#var#} Congratulations! Your registration on Sathi Cab has been successfully completed. Customer Support: {#var#} Thank you for choosing Sathi Cab. Team Sathi Cab Confrer",
   },
+  cabDetails: {
+    templateId: "1207176042979809184",
+    senderId: "SATCAB",
+    text: "Driver Ph.: {#var#}. Cab: No. ({#var#}), Pickup Time & Date {#var#}. You will receive the final amount after completing the journey in Sathi Cab Driver App. Please check initial KMs before trip start. Do not pay before boarding. Sathi Cab - Your trusted travel partner. . Confrere Taxi",
+  },
   bookingConfirmation: {
     templateId: "1207176012592338849",
-    senderId: "Confreretaxi",
+    senderId: "SATCAB",
     text: "Hi {#var#}, your booking (ID: {#var#}) has been confirmed. {#var#} will arrive for your pickup. Sathi Cab. Confrere Taxi",
   },
   paymentConfirmation: {
@@ -24,7 +29,48 @@ const templates = {
     senderId: "SATCAB",
     text: "Remaining Amount: Rs. {#var#} Included Km: {#var#} Extra Km Charge: {#var#} Driver Allowance: Included Toll, State tax, Parking Extra You will soon receive your car and driver details. Important: Sathicab is not responsible for any extra payment apart from above mentioned. Sathi Cab https://Sathicabs.com/ .Confrere Taxi",
   },
+  TripSummary: {
+    templateId: "1207176016646355544",
+    senderId: "SATCAB",
+    text: "Your booking (ID: {#var#}) has been marked completed by driver. Extra Km: {#var#}, Extra Days: {#var#}. Price details will follow. Sathi Cab Confrere Taxi",
+  },
+  TripSummaryDetailed: {
+    templateId: "1207176016790156229",
+    senderId: "SATCAB",
+    text: "Subtotal: {#var#}, GST:5% {#var#}, Total Amount to Pay: {#var#}. Toll and State taxes extra. Thank you for choosing Sathi Cab . Confrere Taxi",
+  },
+  bookingDriverAccepted: {
+    templateId: "1207176019177859542",
+    senderId: "SATCAB",
+    text: "You have successfully accepted booking (ID: {#var#}). Customer: {#var#}, Ph.: {#var#}. Please get ready for pickup. Sathi Cab . Confrere Taxi",
+  },
+  bookingDriverAccepted2: {
+    templateId: "1207176019566546292",
+    senderId: "SATCAB",
+    text: "Car: {#var#} - No.: {#var#}. Pickup Date/Time: {#var#}. For more details visit Sathi Cab App or website. Thank you forhttps://sathicabs.com Sathi Cab . Confrere Taxi",
+  },
+  bookingDriverCompleted: {
+    templateId: "1207176024516774675",
+    senderId: "SATCAB",
+    text: "Booking (ID: {#var#}) has been marked completed. Customer: {#var#}, Ph.: {#var#}. Please verify trip details in Sathi Cab . Confrere Taxi",
+  },
+  bookingDriverCompleted2: {
+    templateId: "1207176024492429666",
+    senderId: "SATCAB",
+    text: "Extra day & KM : {#var#}, Received from Costumer: {#var#}, Deducted from Wallet: {#var#}. Toll and State taxes extra. Thank you for driving with Sathi Cab . Confrere Taxi",
+  },
   // Add all 20 templates here...
+
+  driverRegistration: {
+    templateId: "1207175369879985443",
+    senderId: "SATICB",
+    text: "प्रिय {#var#}, बधाई हो! आपका साथी कैब ड्राइवर के रूप में रजिस्ट्रेशन सफलतापूर्वक पूरा हो गया है। अब आप हमारी एप्लिकेशन में लॉगिन करके राइड्स प्राप्त करना शुरू कर सकते हैं। अगर आपको किसी प्रकार की सहायता चाहिए, तो कृपया हमसे संपर्क करें: सहायता नंबर: {#var#} साथी कैब की टीम की ओर से आपको शुभकामनाएँ! धन्यवाद। – साथी कैब टीम",
+  },
+  userProfileCompleted: {
+    templateId: "1207175394359796303",
+    senderId: "SATICB",
+    text: "Dear {#var#} Congratulations! Your registration on Sathi Cab has been successfully completed. You can now easily book rides anytime, anywhere using our mobile app. Customer Support: {#var#} Thank you for choosing Sathi Cab. We wish you a pleasant journey! Team Sathi Cab Confrer",
+  },
 };
 
 module.exports = { templates };
